ad7431a1b47a61763418b04f6b44ea5235eec475
Mysiak <Patrycja.Mysiak@PerkinElmer.com>
2022-06-08 17:53:49 +0200

    changed Navigate to Redirect and Route to Switch

--- src/screens/Content.js
+++ src/screens/Content.js
@@ -1,16 +1,16 @@
 import Characters from "./Characters"
 import Houses from "./Houses"
-import {Route, Routes, Navigate} from "react-router-dom"
+import {Route, Switch,Redirect} from "react-router-dom"
 import HouseDetails from "./HouseDetails"
 
 
 export default function Content() {
     return (
-        <Routes>
-            <Route path="/" element={<Navigate to="characters" />}></Route>
+        <Switch>
+            <Route path="/" element={<Redirect to="characters" />}></Route>
             <Route path="characters" element={<Characters />}></Route>
             <Route path="houses" element={<Houses />}></Route>
             <Route path="houses/:id" element={<HouseDetails />}></Route>
-        </Routes>
+        </Switch>
     )
 }
